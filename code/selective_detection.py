7f0c2c27808b472d426184f5df1080b6f10a608a
David Khachikov <d.khachikov@innopolis.university>
2024-10-26 10:35:18 +0300

    something

--- code/selective_detection.py
+++ code/selective_detection.py
@@ -1,100 +1,125 @@
 import os
 from ultralytics import YOLO
-from torch import device, cuda, load
 from iou import calculate_iou_diff_models_batch
-from multiprocessing import Manager, shared_memory, Pool, Array
+from multiprocessing import Manager, Pool, Array
 import numpy as np
-from concurrent.futures import ProcessPoolExecutor
 from torch.utils.data import DataLoader, Dataset
 import fiftyone.zoo as foz
 from ctypes import c_double
 import json
 
-device_torch = device("cuda" if cuda.is_available() else "cpu")
-
-def get_weights(n):
-    """
-    Property of weights -- all of them in range [0, 1].
-    """
-    k = 0.5
-    w = np.array([(i / n)**k for i in range(1, n+1)])
-    return w / w.sum()
-
-def load_yolo_checkpoints(n=20, step=5, folder_path=".\\checkpoints", prefix="epoch", yolo_version="yolo11n"):
-    loaded_checkpoints = []
+class EnsembleOfCheckpoints:
+    def __init__(self, n_checkpoints=20, step=5, folder_path=".\\checkpoints", prefix="epoch"):
+        self.n_checkpoints = n_checkpoints
+        self.step = step
+        self.folder_path = folder_path
+        self.prefix = prefix
+        self.checkpoints = self.load_checkpoints()
+        self.weights = self.get_weights(len(self.checkpoints) - 1)
     
-    for i in range(0, n*step, step):
-        checkpoint_name = f"{prefix}{i}.pt"
-        checkpoint_path = os.path.join(folder_path, checkpoint_name)
+    def get_weights(self, n, k = 0.5):
+        """
+        Generates weights for checkpoints such that their sum equals to 1, with a "smoothnest" parameter k.
+        """
+        w = np.array([(i / n)**k for i in range(1, n + 1)])
+        return w / w.sum()
+
+    def load_checkpoints(self):
+        """
+        Loads YOLO checkpoints based on initialized parameters.
+        """
+        loaded_checkpoints = []
         
-        print(f"Loading checkpoint: {checkpoint_path}")
-        loaded_checkpoints.append(YOLO(checkpoint_path))
-    
-    return loaded_checkpoints
-
-def voting(dataloader, checkpoints):
-    weights = get_weights(len(checkpoints)-1)
-    num_checkpoints = len(checkpoints)
-    num_images = len(dataloader.dataset)
-    
-    divergences = np.zeros((num_checkpoints - 1, num_images))
-    
-    last_checkpoint = checkpoints[-1]
-    
-    for i in range(num_checkpoints - 1):
-        checkpoint = checkpoints[i]
+        for i in range(0, self.n_checkpoints * self.step, self.step):
+            checkpoint_name = f"{self.prefix}{i}.pt"
+            checkpoint_path = os.path.join(self.folder_path, checkpoint_name)
+            
+            print(f"Loading checkpoint: {checkpoint_path}")
+            loaded_checkpoints.append(YOLO(checkpoint_path))
         
-        for j, batch in enumerate(dataloader):
-            iou = calculate_iou_diff_models_batch(batch, last_checkpoint, checkpoint)
-            print(iou)
-            divergences[i, j*len(batch):j*len(batch)+len(batch)] = iou
-    return divergences, weights @ divergences
+        return loaded_checkpoints
+    
+    def vote_one_image(self, image_path):
+        """
+        Perform voting with divergence computation between checkpoints and last checkpoint FOR ONLY ONE IMAGE.
+        """
+        num_checkpoints = len(self.checkpoints)
+        
+        divergences = np.zeros((num_checkpoints - 1,))
+        last_checkpoint = self.checkpoints[-1]
+        
+        for i in range(num_checkpoints - 1):
+            checkpoint = self.checkpoints[i]
+            
+            iou = calculate_iou_diff_models_batch([image_path], last_checkpoint, checkpoint)
+            divergences[i] = iou[0]
+        
+        evaluation_results = self.weights @ divergences
+        return divergences, evaluation_results
 
-def process_checkpoint_shared(i, checkpoint, last_checkpoint, dataloader, shared_divergences):
-    def process_checkpoint(i, checkpoint, last_checkpoint, dataloader, verbose=True):
+    def voting(self, dataloader):
+        """
+        Perform voting with divergence computation between checkpoints and last checkpoint.
+        """
+        num_checkpoints = len(self.checkpoints)
+        num_images = len(dataloader.dataset)
+        
+        divergences = np.zeros((num_checkpoints - 1, num_images))
+        last_checkpoint = self.checkpoints[-1]
+        
+        for i in range(num_checkpoints - 1):
+            checkpoint = self.checkpoints[i]
+            
+            for j, batch in enumerate(dataloader):
+                iou = calculate_iou_diff_models_batch(batch, last_checkpoint, checkpoint)
+                print(iou)
+                divergences[i, j * len(batch):j * len(batch) + len(batch)] = iou
+        
+        evaluation_results = self.weights @ divergences
+        return divergences, evaluation_results
+    
+    def _process_checkpoint_shared(self, i, checkpoint, last_checkpoint, dataloader, shared_divergences, num_images):
+        """
+        Helper function for parallel processing of each checkpoint's divergence calculation.
+        """
         divergence = []
         for batch in dataloader:
             iou = calculate_iou_diff_models_batch(batch, last_checkpoint, checkpoint)
             divergence.append(iou)
-            if verbose:
-                print(f"{i} vs last: {iou}")
-        return np.concatenate(divergence)
-    divergence = process_checkpoint(i, checkpoint, last_checkpoint, dataloader)
-    shared_divergences[i, :] = divergence
-    return i
+        shared_divergences[i * num_images:(i + 1) * num_images] = np.concatenate(divergence)
 
-def voting_parallelized_experimental(dataloader, checkpoints):
-    # Get weights and necessary dimensions
-    weights = get_weights(len(checkpoints))
-    num_checkpoints = len(checkpoints)
-    num_images = len(dataloader.dataset)
+    def voting_parallelized(self, dataloader):
+        """
+        Parallelized voting with shared memory divergence storage.
+        """
+        num_checkpoints = len(self.checkpoints)
+        num_images = len(dataloader.dataset)
+        
+        # Shared memory for divergences array
+        shared_divergences = Array(c_double, (num_checkpoints - 1) * num_images)
+        
+        # Shared data for multiprocessing
+        manager = Manager()
+        shared_checkpoints = manager.list(self.checkpoints)
+        shared_dataloader = manager.list(dataloader)
+
+        args = [
+            (i, shared_checkpoints[i], shared_checkpoints[-1], shared_dataloader, shared_divergences, num_images)
+            for i in range(num_checkpoints - 1)
+        ]
+
+        # Using multiprocessing pool for parallel processing
+        with Pool() as pool:
+            pool.starmap(self._process_checkpoint_shared, args)
+
+        # Reshape shared_divergences into final form after processing
+        divergences = np.frombuffer(shared_divergences.get_obj()).reshape((num_checkpoints - 1, num_images))
+
+        # Calculate weighted sum of divergences
+        evaluation_results = self.weights @ divergences
+
+        return divergences, evaluation_results
     
-    # Create a shared Array for storing divergences as a flat list (row-major order)
-    shared_divergences = Array(c_double, (num_checkpoints - 1) * num_images)
-    
-    # Manager lists to share read-only data among processes
-    manager = Manager()
-    shared_checkpoints = manager.list(checkpoints)
-    shared_dataloader = manager.list(dataloader)
-    
-    # Define arguments for each checkpoint's processing
-    args = [
-        (i, shared_checkpoints[i], shared_checkpoints[-1], shared_dataloader, shared_divergences, num_images) 
-        for i in range(num_checkpoints - 1)
-    ]
-
-    # Use multiprocessing.Pool to distribute the computation
-    with Pool() as pool:
-        pool.starmap(process_checkpoint_shared, args)
-
-    # Reshape shared_divergences array after pool processing
-    final_divergences = np.frombuffer(shared_divergences.get_obj()).reshape((num_checkpoints - 1, num_images))
-
-    # Calculate weighted sum of divergences
-    weighted_divergences = np.dot(weights[:-1], final_divergences)
-
-    return final_divergences, weighted_divergences
-
 class DatasetOfPaths(Dataset):
     def __init__(self, filepaths):
         self.filepaths = filepaths
@@ -118,7 +143,7 @@ def get_coco_dataloader(n_images=64, batch_size=8):
     pytorch_dataset = DatasetOfPaths([sample.filepath for sample in coco_dataset])
 
     # Create the DataLoader
-    coco_dataloader = DataLoader(dataset=pytorch_dataset, batch_size=batch_size, shuffle=shuffle)
+    coco_dataloader = DataLoader(dataset=pytorch_dataset, batch_size=batch_size, shuffle=False)
     
     return coco_dataloader
 
@@ -126,6 +151,7 @@ if __name__ == '__main__':
     persentage_of_lowest = 0.2
     n_images = 512
     n_checkpoints = 20
+
 
     checkpoints = load_yolo_checkpoints(n_checkpoints, folder_path='./data/checkpoints/AdamW/')
     dataloader = get_coco_dataloader(n_images)
